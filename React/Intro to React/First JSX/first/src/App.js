514b3f2b92f14d83b183b73a736bb32d45190608
Marah Ashayer <110784795+Marah-Ashayer@users.noreply.github.com>
2022-12-16 11:02:29 +0200

    Add files via upload

---
+++ React/Intro to React/First JSX/first/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = ()=>{
+  return ( 
+  <>
+    <h1> Hello Dojo</h1>
+    <h3> Things I need To Do : </h3>
+    <ul>
+      <li>Learn React</li>
+      <li>Climb Mt.Everest</li>
+      <li>Run a Marathone</li>
+      <li>Feed the Dogs</li>
+    </ul>
+  </>
+  );
+  }
+
+
+export default App;
