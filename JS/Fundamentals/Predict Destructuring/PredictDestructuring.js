7b3e7585487a964b70e1028f66ef11e9c0f53805
Marah Ashayer <110784795+Marah-Ashayer@users.noreply.github.com>
2022-12-16 10:55:28 +0200

    Add files via upload

---
+++ JS/Fundamentals/Predict Destructuring/PredictDestructuring.js
@@ -0,0 +1,70 @@
+// ------------------------------------------ Problem 1 ------------------------------------------------//
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar) // Output : Tesla 
+console.log(otherRandomCar)  // Output : Mercedes
+
+
+// --------------------------------------------- Problem 2 --------------------------------------------//
+
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name); //  Will produce  an Error
+console.log(otherName); // Output :  Elon
+
+
+// -------------------------------------------- Problem 3 -----------------------------------------------//
+
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password); //   Output : 12345
+console.log(hashedPassword); // Error Since there is no key "passward"
+
+// -------------------------------------------- Problem 4 -----------------------------------------------//
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers; // 2
+const [,,,second] = numbers; // 5
+const [,,,,,,,,third] = numbers; // 2
+//Predict the output
+console.log(first == second); // Output : False
+console.log(first == third); // Output : True
+
+// ----------------------------------------------- Problem 5 -----------------------------------------//
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key); // Output :value 
+console.log(secondKey); // Output : [1, 5, 1, 8, 3, 3]
+console.log(secondKey[0]); // Output : 1 
+console.log(willThisWork); // Output : 5
+
+// -------------------------------------------------------------------------------------------------//
+
+
+
+
+
+
